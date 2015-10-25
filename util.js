96196fc3ba2f5e67dbb1b7126fe799a3d9c93ce9
latorrefi <latorrefi@gmail.com>
2015-10-25 18:51:44 +0000

    util.js modified

--- util.js
+++ util.js
@@ -1,10 +1,19 @@
 var passwordLength = 8;
+var validContentType = ["application/json"]
 module.exports = 
 {
 
 isValid : function (password){
     var isValid = true;
     if(password.length > passwordLength){
+        isValid = false;
+    }
+    return isValid;
+},
+
+isValidContentType : function (contentType){
+    var isValid = true;
+    if(validContentType.indexOf(contentType)===-1){
         isValid = false;
     }
     return isValid;
