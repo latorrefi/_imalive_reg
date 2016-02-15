23f8dcf7ff219d87ef5d538a48eecc779615ae8c
latorrefi <latorrefi@gmail.com>
2016-02-15 21:42:38 +0000

    Stable version with promises

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
 var bodyParser = require('body-parser');
+var q = require('q');
+
+
+
 //
 //  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
 //
@@ -15,11 +19,101 @@ var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
 
-var ActiveUsers = require("mongoose/model/activeusers");
+var mongoose = require('mongoose');
+// q
+mongoose.Promise = require('q').Promise;
 
+
+mongoose.connection.close();
+mongoose.connect('mongodb://guest:guest@ds043714.mongolab.com:43714/_imalive_reg_');
+
+
+var ActiveUsers = require("mongoose/model/activeusers").ActiveUsers;
+var UserNumbers = require("mongoose/model/usernumbers").UserNumbers;
 
 router.use(bodyParser.json()); // support json encoded bodies
 //router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+function asyncLoop(iterations, func, callback) {
+    var index = 0;
+    var done = false;
+    var loop = {
+        next: function() {
+            if (done) {
+                return;
+            }
+
+            if (index < iterations) {
+                index++;
+                func(loop);
+
+            } else {
+                done = true;
+                callback();
+            }
+        },
+
+        iteration: function() {
+            return index - 1;
+        },
+
+        break: function() {
+            done = true;
+            callback();
+        }
+    };
+    loop.next();
+    return loop;
+}
+
+function searchAndInsertUserNumber(toAnalyze, callback) {
+    //INIT
+    var query = UserNumbers.findOne({ usernumber: toAnalyze});
+    // selecting the `name` and `occupation` fields
+    query.select('usernumber');
+    // execute the query at a later time
+    query.exec(function (err, usernumber) {
+        if (err){
+          
+        }
+        if(usernumber===null){
+          var number =  new UserNumbers({usernumber:toAnalyze});
+          number.save(function (err) {
+              if (err) {
+                //KO
+              }
+              else{
+                //OK
+              }
+            });
+        }
+    });
+    //END
+    callback();
+}
+
+router.post('/imalive/registeredusers/loadnumbers',function(req, res) {
+  var usernumber_lenght = req.body.usernumber_lenght;
+  var numbers = [];
+  var i = 1;
+  while(i<usernumber_lenght){
+    //Generate a new random Number of ten digit
+    var testUserNumber = {};
+    testUserNumber = Math.floor(1000000000 + Math.random() * 9000000000);
+    numbers.push(testUserNumber);
+    i++;
+  }
+  asyncLoop(numbers.length, function(loop) {
+    searchAndInsertUserNumber(numbers[loop.iteration()], function(result) {
+        // Okay, for cycle could continue
+        loop.next();
+    })},
+    function(){
+                res.statusCode = 204;
+                return res.send();}
+  );
+});
 
 router.post('/imalive/registeredusers',function(req, res) {
     var password = req.body.password;
@@ -27,64 +121,70 @@ router.post('/imalive/registeredusers',function(req, res) {
     var user = {};
     user.password = password;
     user.email = email;
+    var created_user_number;
     if(util.isValid(user)){
-      //Generate a new random Number of ten digit
-      var testUserNumber = {};
-      testUserNumber = Math.floor(1000000000 + Math.random() * 9000000000);
-      // find each ActiveUser with a usernamer :testUserNamber
-      var query = ActiveUsers.findOne({ usernumber: testUserNumber });
+    
+      var query = UserNumbers.findOne({});
       // selecting the `name` and `occupation` fields
       query.select('usernumber');
-
       // execute the query at a later time
-      query.exec(function (err, usernumber) {
-        if (err){
-          res.statusCode = 500;
-    		  return res.send('Internal error');
-        }
-        if(usernumber===null){
-          var newActiveUser =  new ActiveUsers({usernumber:testUserNumber , password: user.password ,mail:user.email  });
-          newActiveUser.save(function (err) {
-            if (err) {
-    		     res.statusCode = 500;
-    		     return res.send('Internal error');
-            }
-            else  {
-      	      res.statusCode = 201;
-      	      return res.send('/imalive/reg/activeusers/'+testUserNumber);
-            }
-          });
-        }
+      var promiseCreateUserNumber = query.exec();
+      promiseCreateUserNumber.then(function(newnumber) {
+        created_user_number = newnumber.usernumber;
+        return newnumber.remove();
+        /*var deferred = q.defer();
+        deferred.resolve(created_user_number);
+        return deferred.promise;*/
+      })
+      .then(function(){
+        console.log('User: ' + created_user_number);
+        var newActiveUser =  new ActiveUsers({usernumber:created_user_number , password: user.password ,mail:user.email  });
+        return newActiveUser.save();
+      })
+      .then(function(newActiveUser){
+         res.statusCode = 201;
+      	 res.setHeader("Access-Control-Allow-Origin", "https://imalive-reg-fe-ghiron.c9users.io");
+      	 res.setHeader("Location", req.protocol + '://' + req.get('host') + req.originalUrl+'/'+newActiveUser.usernumber);
+      	 res.setHeader("Content-Location", req.protocol + '://' + req.get('host') + req.originalUrl+'/'+newActiveUser.usernumber);
+      	 res.setHeader("Content-Type", "application/json;charset=UTF-8");
+      	 
+      	 return res.send(JSON.stringify(newActiveUser));
+      })
+      .catch(function(err){
+        // just need one of these
+        console.log('error:', err);
+        res.statusCode = 404;
+        return res.send('Error 500');
       });
     }else{
         res.statusCode = 400;
-        return res.send('Invalid data');
+        return res.send('Error 400');
     }
-    
 });
 
 
 router.get('/imalive/registeredusers/:usernumber',function(req, res) {
     var search_usernumber = req.params.usernumber;
     var search_password = req.query.password;
-    // find each ActiveUser with a usernamer :testUserNamber
-      var query = ActiveUsers.findOne({ usernumber: search_usernumber,password: search_password});
-      // execute the query at a later time
-      query.exec(function (err, user) {
-        if(err){
-          res.statusCode = 404;
-        }else{
-          if(user===null){
+    var promise =  ActiveUsers.findOne({ usernumber: search_usernumber,password: search_password}).exec();
+    promise.then(function(user) {
+      // do something with updated user
+      if(user===null){
              res.statusCode = 404;
-             return res.send('No user created with usernamber '+search_usernumber);
-          }else{
-            res.statusCode = 200;
-             return res.send(JSON.stringify(user));
-          }
-        }
-      });
-      
-   
+             return res.send('Error 600');
+      }else{
+          console.log('User: ' + user.usernumber);
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json;charset=UTF-8");
+          return res.send(JSON.stringify(user));
+      }
+    })
+    .catch(function(err){
+      // just need one of these
+      console.log('error:', err);
+      res.statusCode = 404;
+      return res.send('Error 500');
+    });
 });
 
 /*router.put('/imalive/registeredusers/:usernumber',function(req, res) {
@@ -112,7 +212,7 @@ router.get('/imalive/registeredusers/:usernumber',function(req, res) {
                   res.statusCode = 200;
                   return res.send(JSON.stringify(user));
                 }
-            })
+            });
             
           }
         }
@@ -126,33 +226,59 @@ router.delete('/imalive/registeredusers/:usernumber',function(req, res) {
     var search_usernumber = req.params.usernumber;
     var search_password = req.query.password;
     var query = ActiveUsers.findOne({ usernumber: search_usernumber,password:search_password});
-      // execute the query at a later time
+    var promiseFindUser = query.exec();
+    promiseFindUser.then(function(user){
+      if(user===null){
+        res.statusCode = 404;
+        return res.send('Error 800');
+      }else{
+        return query.remove();
+      }
+    })
+    .then(function(){
+      res.statusCode = 204;
+      return res.send();
+    })
+    .catch(function(err){
+      // just need one of these
+      console.log('error:', err);
+      res.statusCode = 404;
+      return res.send('Error 700');
+    });
+    /*
+    // execute the query at a later time
       query.exec(function (err, user) {
         if(err){
           res.statusCode = 500;
-          return res.send('Error');
+          return res.send('Error 700');
         }else{
           if(user===null){
              res.statusCode = 404;
-             return res.send('No user with usernamber '+search_usernumber);
+             return res.send('Error 800');
           }else{
             query.remove(function (err) {
                 if (err){
                    res.statusCode = 500;
-                  return res.send('Error Updating');
+                  return res.send('Error 900');
                 }else{ 
                   res.statusCode = 204;
                   return res.send();
                 }
-            })
+            });
             
           }
         }
       });
-      
+      */
    
 });
 
+router.options('/imalive/registeredusers',function(req, res) {
+  res.statusCode = 204;
+  res.setHeader("Access-Control-Allow-Origin", "https://imalive-reg-fe-ghiron.c9users.io");
+  res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+  return res.send();
+});  
 
 router.patch('/imalive/registeredusers/:usernumber',function(req, res) {
     var search_usernumber = req.params.usernumber;
@@ -166,18 +292,18 @@ router.patch('/imalive/registeredusers/:usernumber',function(req, res) {
       query.exec(function (err, user) {
         if(err){
           res.statusCode = 500;
-          return res.send('Error');
+          return res.send('Error 1000');
         }else{
           if(user===null){
              res.statusCode = 404;
-             return res.send('No user with usernamber '+search_usernumber);
+             return res.send('Error 1100');
           }else{
             if(typeof update_user.password !== 'undefined'){
               if(util.isPasswordValid(update_user)){
                 user.password = update_user.password;
               }else{
                 res.statusCode = 400;
-                return res.send('Invalid data');
+                return res.send('Error 1200');
               }
             }
             if (typeof update_user.mail !== 'undefined'){
@@ -185,13 +311,13 @@ router.patch('/imalive/registeredusers/:usernumber',function(req, res) {
                 user.mail = update_user.mail;
               }else{
                 res.statusCode = 400;
-                return res.send('Invalid data');
+                return res.send('Error 1300');
               }
             }
             user.save(function (err) {
                 if (err){
                    res.statusCode = 500;
-                  return res.send('Error Updating');
+                  return res.send('Error 1400');
                 }else{ 
                   res.statusCode = 200;
                   return res.send(JSON.stringify(user));
